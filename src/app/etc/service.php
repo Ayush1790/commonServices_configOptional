9d1d22f82866f00ed8a4537b2c4450091c27f58c
Ayush1790 <ayushgupta@cedcoss.com>
2023-06-26 13:13:51 +0530

    done

---
+++ src/app/etc/service.php
@@ -0,0 +1,70 @@
+<?php
+
+
+use Phalcon\Mvc\View;
+
+use Phalcon\Url;
+use Phalcon\Db\Adapter\Pdo\Mysql;
+use Phalcon\Session\Manager;
+use Phalcon\Http\Response\Cookies;
+
+
+$container->set(
+    'view',
+    function () {
+        $view = new View();
+        $view->setViewsDir(APP_PATH . '/views/');
+        return $view;
+    }
+);
+
+$container->set(
+    'url',
+    function () {
+        $url = new Url();
+        $url->setBaseUri('/');
+        return $url;
+    }
+);
+$container->set(
+    'db',
+    function () {
+     return new Mysql($this["config"]->db->toArrray());
+    }
+);
+
+$container->set(
+    'mongo',
+    function () {
+        $mongo = new MongoClient();
+
+        return $mongo->selectDB('phalt');
+    },
+    true
+);
+$container->set(
+    'session',
+    function () {
+        $session = new Manager();
+        $files = new Stream(
+            [
+                'savePath' => '/tmp',
+            ]
+        );
+
+        $session
+            ->setAdapter($files)
+            ->start();
+
+        return $session;
+    }
+);
+
+$container->set(
+    'cookies',
+    function () {
+        $cookies = new Cookies();
+        $cookies->useEncryption(false);
+        return $cookies;
+    }
+);
---
+++ src/public/index.php
@@ -0,0 +1,58 @@
+<?php
+
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\Url;
+use Phalcon\Db\Adapter\Pdo\Mysql;
+use Phalcon\Config;
+use Phalcon\Config\ConfigFactory;
+
+$config = new Config([]);
+
+// Define some absolute path constants to aid in locating resources
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
+
+// Register an autoloader
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        APP_PATH . "/controllers/",
+        APP_PATH . "/models/",
+    ]
+);
+
+$loader->register();
+
+
+$container = new FactoryDefault();
+
+require_once APP_PATH."/etc/service.php";
+
+$application = new Application($container);
+
+$container->set(
+    'config',
+    function () {
+        $fileName = APP_PATH . '/etc/config.php';
+        $factory  = new ConfigFactory();
+        $config = $factory->newInstance('php', $fileName);
+        return $config;
+    }
+  
+);
+
+
+try {
+    // Handle the request
+    $response = $application->handle(
+        $_SERVER["REQUEST_URI"]
+    );
+
+    $response->send();
+} catch (\Exception $e) {
+    echo 'Exception: ', $e->getMessage();
+}
